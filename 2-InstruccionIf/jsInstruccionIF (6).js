91ed0a56b7f35d8786bc612d43df161e199bac0a
fpicollo <33420808+fpicollo@users.noreply.github.com>
2018-07-10 21:55:16 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,24 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+    edad = document.getElementById("edad").value;
+
+    if (edad >= 18)
+    {
+        alert("Usted es mayor edad")
+    }    
+
+    else if (edad <= 12)
+    {   
+    alert("Usted es menor de edad")
+    }
+    
+    else
+    {
+        alert("Usted es adolescente")
+    }
 
 
 
